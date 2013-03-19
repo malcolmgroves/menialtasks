977ad3a051c31fbc73eec04aeb3f8ec3283e66a6
Malcolm Groves <malcolm.r.groves@gmail.com>
2013-03-19 12:13:31 +1100

    moved model and viewmodel code form a shared package to the projects themselves

--- src/projects/MenialTasks.dpr
+++ src/projects/MenialTasks.dpr
@@ -4,7 +4,13 @@ uses
   FMX.Forms,
   EnumerableAdapter in '..\EnumerableAdapter.pas',
   Views.Main in '..\views\Win\Views.Main.pas' {ViewMain},
-  Views.Task in '..\views\Win\Views.Task.pas' {TaskView};
+  Views.Task in '..\views\Win\Views.Task.pas' {TaskView},
+  Common.Exceptions in '..\common\Common.Exceptions.pas',
+  Model.TaskList in '..\model\Model.TaskList.pas',
+  Model.Task in '..\model\Model.Task.pas',
+  Model.Exceptions in '..\model\Model.Exceptions.pas',
+  ViewModel.Task in '..\viewmodels\ViewModel.Task.pas',
+  ViewModel.Main in '..\viewmodels\ViewModel.Main.pas';
 
 {$R *.res}
 
--- src/tests/MenialTasksTests.dpr
+++ src/tests/MenialTasksTests.dpr
@@ -19,7 +19,10 @@ uses
   Tests.Model.Task in 'model\Tests.Model.Task.pas',
   Tests.Model.TaskList in 'model\Tests.Model.TaskList.pas',
   Tests.ViewModel.Task in 'viewmodels\Tests.ViewModel.Task.pas',
-  Tests.ViewModel.Main in 'viewmodels\Tests.ViewModel.Main.pas';
+  Tests.ViewModel.Main in 'viewmodels\Tests.ViewModel.Main.pas',
+  Model.TaskList in '..\model\Model.TaskList.pas',
+  Model.Task in '..\model\Model.Task.pas',
+  Model.Exceptions in '..\model\Model.Exceptions.pas';
 
 {$R *.RES}
 
